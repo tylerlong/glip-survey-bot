702683c0e7e4799cf27a4ad564770a35b66823ff
Tyler Long <tyler4long@gmail.com>
2018-05-14 17:18:06 +0800

    Try PubNub

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import RingCentral from 'ringcentral-js-concise'
+import PubNub from 'ringcentral-js-concise/src/pubnub'
 import dotenv from 'dotenv'
 
 dotenv.config()
@@ -8,3 +9,8 @@ const rc = new RingCentral('', '', process.env.RINGCENTRAL_SERVER_URL)
 rc.token(JSON.parse(process.env.RINGCENTRAL_TOKEN))
 
 console.log(rc.token())
+
+const pubnub = new PubNub(rc, ['/restapi/v1.0/glip/posts'], message => {
+  console.log(message)
+})
+pubnub.subscribe()
