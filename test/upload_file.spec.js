7dc4ca6f1b28deffac8b71f3dfb42f6bc2c77421
Tyler Long <tyler4long@gmail.com>
2018-05-15 17:18:31 +0800

    Add test code for file uploading

---
+++ test/upload_file.spec.js
@@ -0,0 +1,24 @@
+/* eslint-env jest */
+import RingCentral from 'ringcentral-js-concise'
+import dotenv from 'dotenv'
+import fs from 'fs'
+import path from 'path'
+
+dotenv.config()
+const rc = new RingCentral('', '', process.env.RINGCENTRAL_SERVER_URL)
+rc.token(JSON.parse(process.env.RINGCENTRAL_TOKEN))
+
+describe('demo', () => {
+  test('upload file', async () => {
+    const content = fs.readFileSync(path.join(__dirname, '..', 'result', '230919004.json'))
+    try {
+      const r = await rc.post('/restapi/v1.0/glip/files', content, {
+        groupId: '151764994',
+        name: 'test.json'
+      })
+      console.log(r.data)
+    } catch (e) {
+      console.log(e)
+    }
+  })
+})
