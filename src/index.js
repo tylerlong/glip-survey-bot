7e73be7ae9f023ee93694a8487de68c5f74c4ecc
Tyler Long <tyler4long@gmail.com>
2018-05-15 10:33:02 +0800

    Check mention

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import RingCentral from 'ringcentral-js-concise'
 import PubNub from 'ringcentral-js-concise/src/pubnub'
 import dotenv from 'dotenv'
+import * as R from 'ramda'
 
 dotenv.config()
 const rc = new RingCentral('', '', process.env.RINGCENTRAL_SERVER_URL)
@@ -9,14 +10,20 @@ rc.token(JSON.parse(process.env.RINGCENTRAL_TOKEN))
 ;(async () => {
   const r = await rc.get('/restapi/v1.0/glip/persons/~')
   const botId = r.data.id
+  const mentionBotRegex = new RegExp(`!\\[:Person\\]\\(${botId}\\)`)
 
   const pubnub = new PubNub(rc, ['/restapi/v1.0/glip/posts'], async message => {
     if (message.body.creatorId === botId) {
       return // ignore messages from itself.
     }
+    const groupId = message.body.groupId
+    const r = await rc.get(`/restapi/v1.0/glip/groups/${groupId}`)
+    if (r.data.members.length > 2 && !R.test(mentionBotRegex, message.body.text)) {
+      return // It is a group message which doesn't mention the bot
+    }
     console.log(message)
     try {
-      await rc.post(`/restapi/v1.0/glip/groups/${message.body.groupId}/posts`, { text: 'reply from bot' })
+      await rc.post(`/restapi/v1.0/glip/groups/${groupId}/posts`, { text: `![:Person](${message.body.creatorId}) from the bot` })
     } catch (e) {
       console.log(e.response.data)
     }
---
+++ test/demo.spec.js
@@ -0,0 +1,14 @@
+/* eslint-env jest */
+import RingCentral from 'ringcentral-js-concise'
+import dotenv from 'dotenv'
+
+dotenv.config()
+const rc = new RingCentral('', '', process.env.RINGCENTRAL_SERVER_URL)
+rc.token(JSON.parse(process.env.RINGCENTRAL_TOKEN))
+
+describe('demo', () => {
+  test('hello world', async () => {
+    const r = await rc.get('/restapi/v1.0/subscription')
+    console.log(r.data)
+  })
+})
