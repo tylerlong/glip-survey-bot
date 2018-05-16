b98e81a1e6f264485280b8787e68a0b765ebfce3
Tyler Long <tyler4long@gmail.com>
2018-05-16 13:30:34 +0800

    Simplfy test

--- test/parser.spec.js
+++ test/parser.spec.js
@@ -9,41 +9,5 @@ describe('parser', () => {
     const markdown = fs.readFileSync(path.join(__dirname, '..', '/survey.md'), 'utf-8')
     const result = parse(markdown)
     console.log(JSON.stringify(result, null, 2))
-    expect(result).toEqual([ [ 'h1', {}, 'My awesome survey' ],
-      [ 'p',
-        {},
-        'Hi there, here I am conducting a survey. I will ask you some questions. For each question please choose your answer from a predefined list by replying with 1, 2, 3...etc.' ],
-      [ 'p',
-        {},
-        'At the end of the survey, we will ask you two questions about your t-shirt size and your gender, because we want to offer you a t-shirt for free!' ],
-      [ 'h2', {}, 'Which color do you like?' ],
-      [ 'ul',
-        {},
-        [ 'li', {}, 'Red' ],
-        [ 'li', {}, 'Green' ],
-        [ 'li', {}, 'Blue' ],
-        [ 'li', {}, 'Yello' ] ],
-      [ 'h2', {}, 'Which pet do you like?' ],
-      [ 'ul',
-        {},
-        [ 'li', {}, 'Dog' ],
-        [ 'li', {}, 'Cat' ],
-        [ 'li', {}, 'Fish' ],
-        [ 'li', {}, 'Bird' ] ],
-      [ 'h2', {}, 'Which t-shirt size do you want?' ],
-      [ 'ul',
-        {},
-        [ 'li', {}, 'extra small, XS' ],
-        [ 'li', {}, 'small, S' ],
-        [ 'li', {}, 'medium, M' ],
-        [ 'li', {}, 'large, L' ],
-        [ 'li', {}, 'extra large, XL' ],
-        [ 'li', {}, 'double extra large, XXL' ],
-        [ 'li', {}, 'triple extra large, XXXL' ] ],
-      [ 'h2', {}, 'What is your gender?' ],
-      [ 'ul', {}, [ 'li', {}, 'Male' ], [ 'li', {}, 'Female' ] ],
-      [ 'p', {}, 'You have done the survey! Thank you very much for your input! Reply "download" if you want to see the survey report.' ],
-      [ 'p', {}, 'Bye for now and see you next time!' ]
-    ])
   })
 })
